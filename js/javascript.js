3f000880d39a7fa93bc1b036d69989267fbe49fe
Leandro Magnotti <leandro.magnotti@gmail.com>
2022-08-11 22:48:34 -0300

    Agregando objetos cursos y función visualizar

--- js/javascript.js
+++ js/javascript.js
@@ -4,9 +4,58 @@ function ingresarNombre() {
     let nombre = prompt("Ingrese su nombre");
     while (nombre === "" || nombre === null) {
     nombre = prompt("Ingrese su nombre")};
-    console.log("Bienvenido a la sección Cursos", nombre,", esperamos que estés teniendo un buen día")
     console.log("Bienvenido a la sección Cursos", nombre,", esperamos que estés teniendo un buen día");
     alert("Bienvenido a la sección Cursos " + nombre + ", esperamos que estés teniendo un buen día")
+}
+
+const NeuroBParcial1 = {
+    nombre: "Neuro B Parcial 1",
+    modalidad: "Virtual y Presencial", 
+    duración: "12 hs",
+    precio: "$2500"
+}
+const NeuroBParcial2 = {
+    nombre: "Neuro B Parcial 2",
+    modalidad: "Virtual y Presencial", 
+    duración: "12 hs",
+    precio: "$2500"
+}
+const NeuroBFinal = {
+    nombre: "Neuro B Final",
+    modalidad: "Virtual y Presencial", 
+    duración: "24 hs",
+    precio: "$4000"
+}
+const ExpeParcial1 = {
+    nombre: "Expe Parcial 1",
+    modalidad: "Virtual y Presencial", 
+    duración: "12 hs",
+    precio: "$2500"
+}
+const ExpeParcial2 = {
+    nombre: "Expe Parcial 1",
+    modalidad: "Virtual y Presencial", 
+    duración: "12 hs",
+    precio: "$2500"
+}
+const ExpeFinal = {
+    nombre: "Expe Final",
+    modalidad: "Virtual y Presencial", 
+    duración: "24 hs",
+    precio: "$4000"
+}
+
+function visualizarCursos() {
+    debugger
+    let election; 
+    election = prompt("Si te interesa visualizar los cursos que tenemos disponibles para: \nNeuro B, presioná 1 \nExpe, presioná 2");
+    
+    switch (election) {
+        case "1":
+            alert(NeuroBParcial1.nombre)
+        case "2":
+            alert(ExpeParcial1)
+    }
 }
 
 function demandaYRespuesta() {
@@ -19,9 +68,7 @@ function demandaYRespuesta() {
     alert("Perfecto! el total a abonar es: $" + calculadora(opcionParcial1, opcionParcial2, opcionFinal))
 }
 
-let neuroParcial1 = 2400
-let neuroParcial2 = 2400
-let neuroFinal = 4000 
+
 
 function calculadora(curso1, curso2, curso3) {
     if (curso1 === "1", curso2 === "2", curso3 === "2") {return neuroParcial1}
@@ -30,4 +77,5 @@ function calculadora(curso1, curso2, curso3) {
 } 
 
 ingresarNombre();
+visualizarCursos();
 demandaYRespuesta();
